ce1d5eb88bd57e0ee315ca71427c0a5313ad73c2
Leonel Leon <leonleo1996@gmail.com>
2023-05-06 16:07:07 -0600

    Obteniendo el primer registro con First y FirstOrDefault

--- EFCorePeliculas/Controllers/GenerosController.cs
+++ EFCorePeliculas/Controllers/GenerosController.cs
@@ -20,5 +20,39 @@ namespace EFCorePeliculas.Controllers
         {
             return await context.Generos.ToListAsync();
         }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Genero>> Get(int id)
+        {
+            var genero = await context.Generos.FirstOrDefaultAsync(g => g.Identificador == id);
+
+            if (genero == null)
+            {
+                return NotFound();
+            }
+
+            return genero;
+        }
+
+        [HttpGet("primer")]
+        public async Task<ActionResult<Genero>> Primer()
+        {
+            //De esta forma se trae el primer valor que encuentre en la tabla
+            //return await context.Generos.FirstAsync();
+
+            //Forma para traer el primer valor que empiece con la letra "C"
+            //return await context.Generos.FirstAsync(g => g.Nombre.StartsWith("C"));
+
+            // FirstOrDefault hace que traiga el primero que especifiquemos y si no lo encuentra retorna nulo
+            var genero = await context.Generos.FirstOrDefaultAsync(g => g.Nombre.StartsWith("C"));
+
+            if (genero is null)
+            {
+                return NotFound();
+            }
+
+            return genero;
+
+        }
     }
 }
