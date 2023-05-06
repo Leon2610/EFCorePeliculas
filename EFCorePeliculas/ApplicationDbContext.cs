9b4c1c79bdcb4ae4135015a3461a1e0c8002224a
Leonel Leon <leonleo1996@gmail.com>
2023-05-06 11:29:25 -0600

    Insertando Datos con Data Seeding

--- EFCorePeliculas/ApplicationDbContext.cs
+++ EFCorePeliculas/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using EFCorePeliculas.Entidades;
 using EFCorePeliculas.Entidades.Configuraciones;
+using EFCorePeliculas.Entidades.Seeding;
 using Microsoft.EntityFrameworkCore;
 using System.Reflection;
 
@@ -22,6 +23,7 @@ namespace EFCorePeliculas
             base.OnModelCreating(modelBuilder);
 
             modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            SeedingModuloConsulta.Seed(modelBuilder);
         }
 
         public DbSet<Genero> Generos { get; set; }
