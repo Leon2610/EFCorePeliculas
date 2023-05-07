66a3b4fd7995ed61d9e42f78126373dbfe8bad61
Leonel Leon <leonleo1996@gmail.com>
2023-05-07 12:06:33 -0600

    Ahorrarse el Select con AutoMapper

--- EFCorePeliculas/Controllers/AutoresController.cs
+++ EFCorePeliculas/Controllers/AutoresController.cs
@@ -1,4 +1,6 @@
-﻿using EFCorePeliculas.DTOs;
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using EFCorePeliculas.DTOs;
 using EFCorePeliculas.Entidades;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -10,16 +12,18 @@ namespace EFCorePeliculas.Controllers
     public class AutoresController : ControllerBase
     {
         private readonly ApplicationDbContext context;
+        private readonly IMapper mapper;
 
-        public AutoresController(ApplicationDbContext context)
+        public AutoresController(ApplicationDbContext context, IMapper mapper)
         {
             this.context = context;
+            this.mapper = mapper;
         }
 
         [HttpGet]
         public async Task<IEnumerable<ActorDTO>> Get()
         {
-            return await context.Actores.Select(a => new ActorDTO {Id = a.Id, Nombre = a.Nombre}).ToListAsync();
+            return await context.Actores.ProjectTo<ActorDTO>(mapper.ConfigurationProvider).ToListAsync();
         }
     }
 }
