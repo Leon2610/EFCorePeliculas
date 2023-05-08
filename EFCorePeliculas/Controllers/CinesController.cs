20b4384af7692b661174d08958eab82f1f0c4704
Leonel Leon <leonleo1996@gmail.com>
2023-05-07 22:27:43 -0600

    Consultando Datos Espaciales

---
+++ EFCorePeliculas/Controllers/CinesController.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using EFCorePeliculas.DTOs;
+using EFCorePeliculas.Entidades;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCorePeliculas.Controllers
+{
+    [ApiController]
+    [Route("api/cines")]
+    public class CinesController : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+        private readonly IMapper mapper;
+
+        public CinesController(ApplicationDbContext context, IMapper mapper)
+        {
+            this.context = context;
+            this.mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<CineDTO>> Get()
+        {
+            return await context.Cines.ProjectTo<CineDTO>(mapper.ConfigurationProvider).ToListAsync();
+        }
+    }
+}
