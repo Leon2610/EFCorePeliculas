b492ffed9baa6fea0032115f2e68b839f455ade6
Leonel Leon <leonleo1996@gmail.com>
2023-05-09 21:39:29 -0600

    Filtros al Nivel del Modelo

--- EFCorePeliculas/Entidades/Configuraciones/GeneroConfig.cs
+++ EFCorePeliculas/Entidades/Configuraciones/GeneroConfig.cs
@@ -13,6 +13,8 @@ namespace EFCorePeliculas.Entidades.Configuraciones
             builder.Property(prop => prop.Nombre)
                 .HasMaxLength(150)
                 .IsRequired();
+
+            builder.HasQueryFilter(g => !g.EstaBorrado);
         }
     }
 }
